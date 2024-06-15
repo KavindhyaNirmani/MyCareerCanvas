7eabbacce6867ee097ae7d0e2fba7633afabbe14
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-06-15 23:38:36 +0530

    Recorrected errors

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -3,6 +3,7 @@ import project2 from "../assets/projects/Tour.png";
 import project3 from "../assets/projects/Portfoliyo.png";
 import project4 from "../assets/projects/Weather.png";
 import project5 from "../assets/projects/DeManager.png";
+import project6 from "../assets/projects/Pizza.png";
 
 export const HERO_CONTENT = ``
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
@@ -10,44 +11,52 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Smart_X",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Smart building and components controlling system.",
+    technologies: ["Arduino", "CSS", "React", "C++", "Firebase"],
   },
   {
-    title: "Task Management App",
+    title: "WanderLuxe",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Developed a travel booking website with intuitive search, secure payments, responsive design, and  hotels,  enhancing user experience and simplifying travel planning.",
+    technologies: [ "React", "CSS", "Firebase", "MongoDB"],
   },
   {
-    title: "Portfolio Website",
+    title: "MyCareerCanvas",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["Frame-Motion",  "React", "Tailwind"],
   },
   {
-    title: "Blogging Platform",
+    title: "SkySync",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Created a weather app to check temperature, date, time, wind speed, and humidity, providing accurate and real-time weather updates with a user-friendly interface.",
+    technologies: [ "React", "Tailwind"],
   },
   {
-    title: "Blogging Platform",
+    title: "De-Manager",
     image: project5,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Developed a vehicle service appointment booking system with easy scheduling, real-time availability, and user-friendly interface, streamlining the process for customers and service providers.",
+    technologies: ["Reaact", "Tailwind", "MongoDB", "Node.js"],
   },
+  {
+    title: "TastyToppings",
+    image: project6,
+    description:
+      "Streamline pizza orders with our user-friendly app, offering a variety of toppings, real-time tracking, and exclusive deals. Enjoy the convenience of customized pizzas delivered right to your door.",
+    technologies: ["React", "CSS", "Node.js", "MongoDB"],
+  },
+
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "315/6, Yatalamatta, Galle ",
+  phoneNo: "+94769670698 ",
+  email: "kavindhyaliyanage24@gmail.com",
 };
